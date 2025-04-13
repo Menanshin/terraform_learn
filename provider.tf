cee1c4a79a58488ba6eb799ee5f6936821ad0d16
Evgeniy Maslov <120675394+Menanshin@users.noreply.github.com>
2025-04-13 19:01:34 +0300

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,22 @@
+# Объявление провайдера
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 1.00"
+}
+
+provider "yandex" {
+  zone                     = "ru-central1-a"
+  folder_id                = "<folder_id>"
+}
+
+provider "aws" {
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
